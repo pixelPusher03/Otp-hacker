848a715f00fd22bce32ff10993000f8f9806e674
ᴘɪxᴇʟ Pᴜsʜᴇʀ03 <153280672+pixelPusher03@users.noreply.github.com>
2024-11-28 04:29:47 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,60 @@
+# WhatsApp OTP Hacker
+# This is a Python script used to bypass whatsapp OTP verification process
+This script was created by the developer😠 and the developer ofc
+
+## <!-- Typing SVG -->
+<p align="center">
+    <a href="https://github.com/47hxl-53r">
+        <img
+src="https://readme-typing-svg.herokuapp.com/?size=35&width=800&lines=Wa+OPT+Hacker+tool+by+the+developer"
+            alt="Typing SVG"
+        />
+    </a>
+</p>
+
+![Robots](https://github.com/thedeveloper03/Whatsapp-Hacker/assets/123274423/737d00d4-54aa-4d0e-9380-b86c06544fb7)
+
+# WhatsApp OTP Hacker
+
+# INSTALLATION
+
+<!-- Installation via Termux -->
+<b><details><summary>Installation on Termux</summary></b>
+```bash
+> apt update
+> apt upgrade
+> pkg install python
+> pkg install git -y
+> git clone https://github.com/thedeveloper03/WaHack
+> cd WaHack
+> python3 main.py
+```
+</details>
+
+## screenshots
+![Screenshot_20231001-233110](https://github.com/thedeveloper03/WaHack/assets/123274423/90e13d89-da3f-42b5-b922-f02f9a389750)
+
+# CONTACT
+Click [here](https://bio.link/the_developer) to contact me
+
+# REQUIREMENTS
+> Linux, Termux, Windows
+
+> Follow Me On All my Accounts
+
+> Phone number with country code
+
+# ACCESS CODE
+You will be provided an Access code
+You need to verify it to access the script
+
+# DISCLAIMER ⚠️
+This tool is only created for educational purposes
+
+Iam not responsible for any actions took by users
+
+Iam not responsible for any DAMAGE caused to people using this script
+
+Im not responsible for any DAMAGE that you might encounter. Use At own risk.
+
+## sm technologies official
---
+++ main.py
@@ -0,0 +1,56 @@
+import colorama
+import time, sys
+from colorama import Fore, Style
+Green = Fore.GREEN
+Yellow =Fore.YELLOW
+Blue = Fore.BLUE
+def show_banner() :
+    banner = print(f'''
+{Fore.RED}
+╔╗╔╗───╔╗─  ╔╦═╦╗╔╗─────╔╗─╔═╗──────────
+║╚╝║╔═╗║╠╗  ║║║║║║╚╗╔═╗─║╚╗║═╣╔═╗─╔═╗╔═╗
+║╔╗║║═╣║═╣  ║║║║║║║║║╬╚╗║╔╣╠═║║╬╚╗║╬║║╬║
+╚╝╚╝╚═╝╚╩╝  ╚═╩═╝╚╩╝╚══╝╚═╝╚═╝╚══╝║╔╝║╔╝
+──────────  ──────────────────────╚╝─╚╝─
+    
+    MADE BY THE DEVELOPER😠
+    UPDATED 2023 VERSION 
+ https://www.youtube.com/@smtechmods
+    
+CHECT FOR OTP IN Downloads/OTP/Docker
+    ''')
+show_banner()
+def delay_print(s) :
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.095)
+def num():
+    while True:
+        number = input("Enter number to hack : +1")
+        numberr = "+1" +number
+        lenfind = len(number)
+        if (number.isnumeric()):
+            if (lenfind < 10):
+                print(f"{Fore.RED}Number must be at least 10 digits")
+            elif (lenfind > 10):
+                print(f"{Fore.RED}Number must be less than 10 digits")
+            elif (lenfind==10):
+                delay_print(f"{Fore.YELLOW}Signalling " + numberr + " . . .\n")
+                delay_print(f"{Fore.YELLOW}Checking the network availability . . .\n")
+                delay_print("Connection successfull with " + number+"\n")
+                print(f"{Fore.BLUE}****MAKE SURE YOU REQUESTED THE OTP****\n")
+                delay_print(f"{Fore.YELLOW}Gaining access to SMS . . . ( This will take some time )\n")
+                time.sleep(3)
+                delay_print(f"{Fore.GREEN}Access gained to SMS of " + numberr+"\n")
+                delay_print(f"{Fore.YELLOW}Reading SMS . . .\n")
+                print(f"{Fore.GREEN}Whatsapp OTP found ! !\n")
+                otp = input("Press '1' to show the OTP : ")
+                print(f"{Fore.CYAN}USE THE OTP CODE AND LOGIN\n")
+                print(f"{Fore.RED}Follow and Support Me By Subscribing and following on Instagram. . . ")
+            else:
+                print("Unexpected error occured Contact Me telegram @the_developer01 for a solution")
+        else: print(f"{Fore.RED}Number format is incorrect")
+        num()
+
+num()
